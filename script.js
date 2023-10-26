bce1f772fffdc4f908051bf0161732591904756a
Elba <elba.duishekeeva16@gmail.com>
2023-10-26 20:30:50 +0600

    js2

---
+++ script.js
@@ -0,0 +1,56 @@
+// 1
+// let a = 'hello'
+// let b = 'Ivan'
+// console.log(a.slice(0,1).toUpperCase() + a.slice(1)+ ' '+b);
+
+// 2
+// let a = 'lorem ipsum dolor'
+// let b=a.length;
+// console.log(b);
+
+// 3
+// let a = 'lorem ipsum dolor sit amet'
+// console.log(a.slice(18,21)+' '+a.slice(0,17)+' '+a.slice(a.indexOf('a')))
+
+// 4 вернуть CHILDREn
+// let a = 'children'
+// console.log(a.slice(0,7).toUpperCase() + a.slice(-1))
+
+// 5 вернуть bsso,
+// let a = 'boss'
+// console.log(a.slice(0,1)+a.slice(-2)+a.slice(1,2))
+
+// 6 вернуть index d
+// let a = 'sunday'
+// console.log(a.indexOf('d'))
+
+// 7 вернуть hekso
+// let a = 'hello'
+// console.log(a.replace(a.slice(2,4), 'ks'))
+
+// 8 вернуть new us
+// let a = 'user'
+// let b = 'new'
+// console.log(b+' '+a.slice(0,2))
+
+// 9 вернуть daydaymon
+// let a = 'monday'
+// let b = 'day'
+// console.log(a.substr(3,3)+b+a.substr(0,3));
+
+// 10
+// let a = 'HELLO'
+// console.log(a.slice(0,1).toLowerCase()+a.slice(1,2)+a.slice(3,4).toLowerCase()+a.slice(3,4)+a.slice(-1).toLowerCase())
+
+// 11 вернуть '450'
+// let a = 5
+// let b = '400'
+// console.log(b.replace('0',a))
+
+// 12 вернуть manak
+// let a = 'monkey'
+// let b = 'banana'
+// console.log(a.slice(0,1)+b.slice(-3)+a.slice(3,4))
+
+
+
